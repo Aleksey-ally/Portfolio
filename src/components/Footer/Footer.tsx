ff64224b3a0b9705d1008e507b8bff79b29eb327
Aleksey <easy.mail.to.copy@gmail.com>
2025-01-05 22:06:02 +0300

    refactor: reworked footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,33 +1,9 @@
 import s from './Footer.module.scss';
-import {Title} from "common/components/title/Title";
 
 export const Footer = () => {
     return (
         <div className={s.footerBlock}>
-            <Title subtitle="Footer" title="Fedorov Aleksey"/>
-            <div className={s.icons}>
-                <img
-                    className={s.icon}
-                    src="https://img.icons8.com/?size=512&id=118557&format=png"
-                    alt="#"
-                />
-                <img
-                    className={s.icon}
-                    src="https://img.icons8.com/?size=512&id=118557&format=png"
-                    alt="#"
-                />
-                <img
-                    className={s.icon}
-                    src="https://img.icons8.com/?size=512&id=118557&format=png"
-                    alt="#"
-                />
-                <img
-                    className={s.icon}
-                    src="https://img.icons8.com/?size=512&id=118557&format=png"
-                    alt="#"
-                />
-            </div>
-            <p>Copyright © 2023 All Rights Reserved.</p>
+            <p>Designed and developed by Aleksey Fedorov 2025</p>
         </div>
     );
 };
