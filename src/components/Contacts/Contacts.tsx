335496a4dbca77cb6a47872358d800b7cff25610
Aleksey <easy.mail.to.copy@gmail.com>
2025-01-05 16:01:36 +0300

    feat: added axios and made request after submit form

--- src/components/Contacts/Contacts.tsx
+++ src/components/Contacts/Contacts.tsx
@@ -1,12 +1,29 @@
 import s from './Contacts.module.scss';
 import {Title} from "common/components/title/Title";
+import {FormEvent} from "react";
+import axios from "axios";
+import {Simulate} from "react-dom/test-utils";
+import error = Simulate.error;
+
+const handleSubmit = (a: FormEvent<HTMLFormElement>) => {
+    a.preventDefault()
+    const form = a.currentTarget
+    const formData = new FormData(form);
+    const data: Record<string, string> = {};
+    formData.forEach((value, key) => {
+        data[key] = value.toString();
+    });
+    axios.post('https://smtp-backend-pearl.vercel.app/sendMessage', {data})
+        .then(()=>alert("Your message has been sent"))
+        .catch((error) => alert(error))
+}
 
 export const Contacts = () => {
     return (
         <div className={s.contactsBlock}>
             <Title className={s.title} subtitle="Contact" title="Contact With Me"/>
             <div className={s.formContainer}>
-                <form className={s.form}>
+                <form className={s.form} onSubmit={handleSubmit}>
                     <div className={s.inputGroup}>
                         <label htmlFor="name">Your Name</label>
                         <input type="text" id="name" name="name"/>
@@ -19,7 +36,7 @@ export const Contacts = () => {
 
                     <div className={s.inputGroup}>
                         <label htmlFor="message">Your Message</label>
-                        <textarea id="message" name="message"  rows={5}></textarea>
+                        <textarea id="message" name="message" rows={5}></textarea>
                     </div>
 
                     <button type="submit" className={s.submitButton}>
