f26511015ed88630085596390fbb3cbb2a953e53
Aleksey <easy.mail.to.copy@gmail.com>
2025-01-05 20:43:52 +0300

    feat: added social icons for contacts block

--- src/components/Contacts/Contacts.tsx
+++ src/components/Contacts/Contacts.tsx
@@ -4,6 +4,10 @@ import {Title} from "common/components/title/Title";
 import axios from "axios"
 import {SubmitHandler, useForm} from "react-hook-form";
 import {toast, ToastContainer} from "react-toastify";
+import tlg from "assets/svg/telegram.svg"
+import gmail from "assets/svg/gmail.svg"
+import github from "assets/svg/github.svg"
+import linkedIn from "assets/svg/linkedIn.svg"
 
 type formValues = {
     name: string
@@ -38,29 +42,42 @@ export const Contacts = () => {
     return (
         <div className={s.contactsBlock}>
             <Title className={s.title} subtitle="Contact" title="Contact With Me"/>
-            <div className={s.formContainer}>
-                <form className={s.form} onSubmit={handleSubmit(onSubmit)}>
-                    <div className={s.inputGroup}>
-                        <label htmlFor="name">Your Name</label>
-                        <input {...register("name")} type="text" id="name"/>
-                    </div>
+            <div className={s.container}>
 
-                    <div className={s.inputGroup}>
-                        <label htmlFor="email">Email</label>
-                        <input {...register("email", {required: true})} type="email" id="email"/>
-                        {errors.email && <span className={s.error}>This field is required</span>}
-                    </div>
+                    <form className={s.form} onSubmit={handleSubmit(onSubmit)}>
+                        <div className={s.inputGroup}>
+                            <label htmlFor="name">Your Name</label>
+                            <input {...register("name")} type="text" id="name"/>
+                        </div>
 
-                    <div className={s.inputGroup}>
-                        <label htmlFor="message">Your Message</label>
-                        <textarea {...register("message", {required: true})} id="message" rows={5}></textarea>
-                        {errors.message && <span className={s.error}>This field is required</span>}
-                    </div>
+                        <div className={s.inputGroup}>
+                            <label htmlFor="email">Email</label>
+                            <input {...register("email", {required: true})} type="email" id="email"/>
+                            {errors.email && <span className={s.error}>This field is required</span>}
+                        </div>
 
-                    <button type="submit" className={s.submitButton}>
-                        <span>Send Message</span>
-                    </button>
-                </form>
+                        <div className={s.inputGroup}>
+                            <label htmlFor="message">Your Message</label>
+                            <textarea {...register("message", {required: true})} id="message" rows={5}></textarea>
+                            {errors.message && <span className={s.error}>This field is required</span>}
+                        </div>
+
+                        <button type="submit" className={s.submitButton}>
+                            <span>Send Message</span>
+                        </button>
+                    </form>
+
+
+                <div className={s.social}>
+                    <p>Or contact me in a way more convenient for you</p>
+                    <span className={s.mail}>easy.mail.to.copy@gmail.com</span>
+                    <ul className={s.socialICons}>
+                        <li><a href="#eswrg"><img src={tlg} alt="#"/></a></li>
+                        <li><a href="#eswrg"><img src={gmail} alt="#"/></a></li>
+                        <li><a href="#eswrg"><img src={github} alt="#"/></a></li>
+                        <li><a href="#eswrg"><img src={linkedIn} alt="#"/></a></li>
+                    </ul>
+                </div>
             </div>
             <ToastContainer/>
         </div>
