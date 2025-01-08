f38ad7493eac86e569ec81aa44a156343c4d618e
Aleksey <easy.mail.to.copy@gmail.com>
2025-01-08 21:40:20 +0300

    feat: added effect sticky after scrolling and adapted for different header resolutions

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,9 +1,23 @@
 import {Nav} from './Nav/Nav';
-import s from './Header.module.css';
+import s from 'components/Header/Header.module.scss';
+import {useEffect, useState} from "react";
 
 export const Header = () => {
+
+    const [isSticky, setIsSticky] = useState(false);
+
+    useEffect(() => {
+        const handleScroll = () => {
+            setIsSticky(window.scrollY > 0);
+        };
+
+        window.addEventListener("scroll", handleScroll);
+        return () => {
+            window.removeEventListener("scroll", handleScroll);
+        };
+    }, [isSticky]);
     return (
-        <div className={s.header}>
+        <div className={`${s.header} ${isSticky ? s.sticky : ""}`}>
             <Nav/>
         </div>
     );
